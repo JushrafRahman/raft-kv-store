6e0f416346f4de50033f21710640df1efd0c309c
JushrafRahman <jushrafrahi97@gmail.com>
2025-01-01 19:06:12 -0500

    fix missing log entry

---
+++ build/src/CMakeFiles/raft_lib.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/jushraf/Desktop/dev/raft-kv-store/src/main.cpp" "src/CMakeFiles/raft_lib.dir/main.cpp.o" "gcc" "src/CMakeFiles/raft_lib.dir/main.cpp.o.d"
+  "/Users/jushraf/Desktop/dev/raft-kv-store/src/network/network.cpp" "src/CMakeFiles/raft_lib.dir/network/network.cpp.o" "gcc" "src/CMakeFiles/raft_lib.dir/network/network.cpp.o.d"
+  "/Users/jushraf/Desktop/dev/raft-kv-store/src/node/node.cpp" "src/CMakeFiles/raft_lib.dir/node/node.cpp.o" "gcc" "src/CMakeFiles/raft_lib.dir/node/node.cpp.o.d"
+  "/Users/jushraf/Desktop/dev/raft-kv-store/src/raft/raft.cpp" "src/CMakeFiles/raft_lib.dir/raft/raft.cpp.o" "gcc" "src/CMakeFiles/raft_lib.dir/raft/raft.cpp.o.d"
+  "/Users/jushraf/Desktop/dev/raft-kv-store/src/storage/storage.cpp" "src/CMakeFiles/raft_lib.dir/storage/storage.cpp.o" "gcc" "src/CMakeFiles/raft_lib.dir/storage/storage.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/raft_lib.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raft_lib.dir/main.cpp.o"
+  "CMakeFiles/raft_lib.dir/main.cpp.o.d"
+  "CMakeFiles/raft_lib.dir/network/network.cpp.o"
+  "CMakeFiles/raft_lib.dir/network/network.cpp.o.d"
+  "CMakeFiles/raft_lib.dir/node/node.cpp.o"
+  "CMakeFiles/raft_lib.dir/node/node.cpp.o.d"
+  "CMakeFiles/raft_lib.dir/raft/raft.cpp.o"
+  "CMakeFiles/raft_lib.dir/raft/raft.cpp.o.d"
+  "CMakeFiles/raft_lib.dir/storage/storage.cpp.o"
+  "CMakeFiles/raft_lib.dir/storage/storage.cpp.o.d"
+  "libraft_lib.a"
+  "libraft_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raft_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
